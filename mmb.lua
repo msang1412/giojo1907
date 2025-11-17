58efe565193edc947a90abc117578e6c45593d72
msang1412 <yiweiho881@gmail.com>
2025-11-17 12:03:41 +0700

    Update mmb.lua

--- mmb.lua
+++ mmb.lua
@@ -10,7 +10,7 @@ getgenv().modefarm = getgenv().modefarm or "Normal"
 -- Cấu hình tự động dựa trên modefarm
 getgenv().config = {
     autoFarm = true,
-    flySpeed = 27,
+    flySpeed = 25,
     autoTeleport = true,
     teleportCooldown = 300,
     antiAFK = true,
